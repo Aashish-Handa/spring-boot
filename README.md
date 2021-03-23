07bc421c59719a1b209bd7732517fd543a29e726
Handa, Ashish (ah552m) <ah552m@att.com>
2021-03-23 19:28:42 +0530

    [GIT] Updated packaging structure for the project

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-boot
-Consists of practice projects using springboot
--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practice.springboot;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public abstract class UserRepository implements CrudRepository<User,Long> {
-
-}
